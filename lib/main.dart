541f78f27d972e8ee26e089d02c0b82c8a1e2bba
Tamerlan <t.dukov79@gmail.com>
2026-01-22 16:57:35 +0300

    feat: create layered architecture folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:quick_doodle/firebase_options.dart';
+import 'package:quick_doodle/core/config/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
