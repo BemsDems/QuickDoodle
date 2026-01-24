fbce3b8dd9c9a225a19e828d5e5580793f7808f0
Tamerlan <t.dukov79@gmail.com>
2026-01-24 13:09:57 +0300

    feat: add app icon and splash screen

--- lib/shared/components/custom_scaffold.dart
+++ lib/shared/components/custom_scaffold.dart
@@ -11,7 +11,7 @@ class CustomScaffold extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/images/splash.png"),
+          image: AssetImage("assets/images/background.png"),
           fit: BoxFit.cover,
         ),
       ),
