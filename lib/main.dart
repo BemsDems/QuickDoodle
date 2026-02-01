9908f98caa1a1930760e8ef9dc83bca486fd07d7
Tamerlan <t.dukov79@gmail.com>
2026-02-01 17:32:09 +0300

    fix: delete unuse dependencies from pubspec and add condition for initialize on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_doodle/core/config/firebase_options.dart';
@@ -23,7 +22,7 @@ Future<void> main() async {
     FirebaseDatabase.instance.setPersistenceEnabled(true);
     NotificationService.initialize();
   } catch (e) {
-    log('Ошибка при запуске ${e.toString()}');
+    if (kDebugMode) print('Ошибка при запуске ${e.toString()}');
   }
 
   runApp(ProviderScope(child: const MyApp()));
